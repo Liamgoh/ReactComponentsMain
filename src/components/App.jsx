9831f44c2c54e938eb57b679e62f9f14cf80d0c3
Liamgoh <64313674+Liamgoh@users.noreply.github.com>
2021-09-19 22:38:48 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading.jsx"; //jsx extension optional
+import UnsortedList from "./UnsortedList.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading></Heading>
+      <UnsortedList></UnsortedList>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+//import React as we are using JSX to insert html elements in JavaScript (see below)
+import React from "react";
+
+//React convention to give React component names Pascal case. Components are written in JSX (React):
+function Heading() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+export default Heading;
